d912c5e2948cba477bf07ddabb0149d7912265ef
kmelekhov <114863558+kmelekhov@users.noreply.github.com>
2023-11-25 11:46:06 +0300

    Update init.sql

--- init.sql
+++ init.sql
@@ -1,5 +1,2 @@
--- Необходимо создать пользователя 'test' с паролем 'test'
 CREATE USER test WITH PASSWORD 'test';
-
--- Необходимо создать базу данных 'test' с владельцем 'test'
 CREATE DATABASE test OWNER test;
